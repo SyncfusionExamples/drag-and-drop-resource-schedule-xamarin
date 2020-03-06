0658e1a7e0ca3436a8def06f882ffb3f29ab88fd
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-06 15:20:26 +0530

    XAMARIN-33496 - Sample added.

---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/Behavior/SchedulerPageBehavior.cs
@@ -0,0 +1,41 @@
+﻿using Syncfusion.SfSchedule.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class SchedulerPageBehavior : Behavior<ContentPage>
+    {
+        SfSchedule schedule;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.schedule = bindable.Content.FindByName<SfSchedule>("schedule");
+            this.WireEvents();
+        }
+
+        private void WireEvents()
+        {
+            this.schedule.AppointmentDrop += Schedule_AppointmentDrop;
+        }
+
+        private void Schedule_AppointmentDrop(object sender, AppointmentDropEventArgs e)
+        {
+            var changedResource = e.DropResourceItem;
+            App.Current.MainPage.DisplayAlert("", "Resorce change into " + (changedResource as Employee).Name, "ok");
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            this.UnWireEvents();
+        }
+
+        private void UnWireEvents()
+        {
+            this.schedule.AppointmentDrop -= Schedule_AppointmentDrop;
+        }
+    }
+}
---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/Model/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class Employee
+    {
+        /// <summary>
+        /// Gets or sets name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets id
+        /// </summary>
+        public object Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets Color
+        /// </summary>
+        public Color Color { get; set; }
+
+        /// <summary>
+        /// Gets or sets display picture
+        /// </summary>
+        public string DisplayPicture { get; set; }
+    }
+}
